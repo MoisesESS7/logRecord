6c074a3ead30c7e1366a295b97bceea4bb1c85b2
Moise Silva <moisessilvagsp@outlook.com>
2023-11-05 12:39:12 -0300

    registro de log a partir da leiture de um arquivo

---
+++ LogRecord/Entities/LogRecord.cs
@@ -0,0 +1,25 @@
+﻿namespace Log.Entities
+{
+    internal class LogRecord
+    {
+        public string? UserName { get; set; }
+        public DateTime Instant { get; set; }
+
+        public LogRecord(string line)
+        {
+            string[] vect = line.Split(' ');
+            UserName = vect[0];
+            Instant = DateTime.Parse(vect[1]);
+        }
+
+        public override int GetHashCode() => UserName.GetHashCode();
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+
+            LogRecord? other = obj as LogRecord;
+            return UserName.Equals(other?.UserName);
+        }
+    }
+}
---
+++ LogRecord/Program.cs
@@ -0,0 +1,23 @@
+﻿using Log.Entities;
+
+Console.Write("Enter file full path: ");
+string? path = Console.ReadLine();
+
+HashSet<LogRecord>? users = new HashSet<LogRecord>();
+
+try
+{
+    using(StreamReader sr = File.OpenText(path))
+    {
+        while(!sr.EndOfStream)
+        {
+            string? line = sr.ReadLine();
+            users.Add(new LogRecord(line));
+        }
+        Console.WriteLine("Total users: " + users.Count);
+    }
+}
+catch(IOException e)
+{
+    Console.WriteLine(e.Message);
+}
